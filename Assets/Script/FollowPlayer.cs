320135bf6b7e697de45bac9a7a3b12fff998bc9d
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2022-09-16 12:26:48 -0400

    Fixing camera jigger lol

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset = new Vector3(0, 5, -8);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +13,8 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 5 -7);
+        transform.position = player.transform.position + offset;
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
     public float speed = 20;
     // Start is called before the first frame update
     void Start()
